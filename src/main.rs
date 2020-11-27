5b2f17f66701222e3d519d2192e88a7992da53f1
David Urvoy <david.urvoy.pro@gmail.com>
2020-11-27 21:19:27 +0000

    Basic transactions.csv file read

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+use std::env;
+use std::error::Error;
+use std::fs;
+use std::process;
+
+use csv;
+
 fn main() {
-    println!("Hello, world!");
+
+    let args: Vec<String> = env::args().collect();
+    let input_transactions = &args[1];
+
+    let content = fs::read_to_string(input_transactions)
+        .expect(&format!("Could not read file: {}", input_transactions));
+
+    if let Err(err) = example(&content.as_bytes()) {
+        println!("error running example: {}", err);
+        process::exit(1);
+    }
 }
+
+fn example(transactions: &[u8]) -> Result<(), Box<dyn Error>> {
+    let mut rdr = csv::Reader::from_reader(transactions);
+    for result in rdr.records() {
+        let record = result?;
+        println!("{:?}", record);
+    }
+    Ok(())
+}
+
